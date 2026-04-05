10cf6baece69958ef166a553da3a234036c042de
Paulo Patricio <paulocdn99@gmail.com>
2026-04-05 15:55:29 -0300

    Organizacao da main 1.0

--- src/main.py
+++ src/main.py
@@ -1,56 +1,39 @@
 from algoritmo import new_population, rosenbrock, sphere, rastrigin
-
+import random
 from utils import crossing, mutation, cut, nova_mutacao
 
-# Ponto de entrada/Código principal
 
-variavel = new_population(10,10,-100,101)
+newpopulation = new_population(10,30,-100,100)
+fitness = []
+populacao_avaliada = []
+o = 0
 
-print(variavel)
+#Criando a lista do fitness
+for i in newpopulation:
+    fitness.append(sphere(i))
+    populacao_avaliada.append({
+       
+    "individuo": newpopulation[o],
+    "fitness": fitness[o]
 
-# Implementação da função fitness rosenbrock: 
-
-# 1. Cria a população com os parâmetros corretos:
-# tamanho da população = 30, dimensões = 30, limites = -5.12 a 5.12)
-minha_populacao = new_population(30, 30, -5.12, 5.12)
-
-# 2. Separar um indivíduo da população para testar vamo pegar o primeiro
-individuo_bom = minha_populacao[0]
-
-# 3. Agora passamos esse indivíduo para a função
-resultado_fitness = rosenbrock(individuo_bom)
-
-# 4. Imprimir apenas um indivíduo:
-print("Tamanho do indivíduo:", len(individuo_bom))
-print("Nota desse indivíduo:", resultado_fitness)
-
-# 5. Para imprimir todos os indivíduos:
-for i in range(len(minha_populacao )):
-    individuo_atual = minha_populacao[i]
-    nota = rosenbrock(individuo_atual)
-    print(f" O Indivíduo {i} tirou a nota: {nota:.0f}")
+    })
+    o += 1
 
 
+populacao_selecionada = []
 
+#Selecionando os individuos mais aptos
+for i in range(len(fitness)):
+    # escolhe 2 indivíduos aleatórios
+    a, b = random.sample(populacao_avaliada, 2)
 
+    # compara fitness (menor é melhor no sphere)
+    if a["fitness"] < b["fitness"]:
+        vencedor = a
+    else:
+        vencedor = b
 
-#Implementação da função Sphere
-print("\nFitness da função Sphere: \n")
-test_sphere = new_population(30,10,-5,5)
+    populacao_selecionada.append(vencedor)
 
-for i in range (30):
-   # Usei o f-string para o print ficar limpo e sem espaços extras
-   print(f"{i+1}° Valor ({sphere(test_sphere[i])})")
+print(populacao_selecionada)
 
-
-
-
-
-#Implementação da função Rastrigin
-print("\nFitness da função Rastrigin: \n")
-
-test_rastrigin = new_population(30, 10, -5, 5)
-
-for i in range(30):
-    # Usei o f-string para o print ficar limpo e sem espaços extras
-    print(f"{i+1}° Valor ({rastrigin(test_rastrigin[i])})")
